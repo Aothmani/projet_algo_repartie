8c610d85766724b96cb1050654809b5fb304f38b
Nathan Maurice <nathan.maurice@protonmail.com>
2019-04-03 13:14:29 +0200

    Mise a jour headers

--- Makefile
+++ Makefile
@@ -1,11 +1,17 @@
 
 all: chord
 
-chord: chord.o
+chord: chord.o utils.o
+	mpicc -g $^ -o $@
+
+test: test.o chord.o utils.o
 	mpicc -g $^ -o $@
 
 run: chord
 	mpirun --oversubscribe -np 5 $<
+
+run-test:
+	mpi-run --oversubscribe -np 5 $<
 
 %.o: %.c
 	mpicc -g -c $^ -o $@
--- utils.h
+++ utils.h
@@ -1,6 +1,8 @@
 #ifndef CHORD_CONSTANTS
 #define CHORD_CONSTANTS
 
+#include <mpi.h>
+
 enum Tags {
 	   TAGINIT, /* Sent by the simulator to define the variables */
 	   TAGSEARCH, /* Searching a value in the hash table */
@@ -14,11 +16,21 @@ enum Tags {
 	   TAGUPDATE, /* Tell a node to update its fingers in regards to the newly inserted node */
 	   TAGUPDATERESP, /* Response to TAGTELLFINGER, contains the new finger */
 	   TAGINSERT, /* Insert a node in the ring */
-	   TAGINSERTRESP /* Response of TAGINSERT, indicate that the node has been inserted in the ring */
+	   TAGINSERTRESP, /* Response of TAGINSERT, indicate that the node has been inserted in the ring */
+	   TAGTERM, /* Terminate a node */
 };
 
+#define SEND_INT(dest, tag, data) MPI_Send((void*)&data, 1, MPI_INT, dest, tag, MPI_COMM_WORLD)
+#define SEND_NINT(dest, tag, data, n) MPI_Send((void*)data, n, MPI_INT, dest, tag, MPI_COMM_WORLD)
+/*
+ * find_finger - Determines the finger to which the request should be transmitted to
+ * @fingers finger table
+ * @m size of finger table
+ * @wanted wanted value
+ */
+void find_finger(const int* fingers, int m, int wanted);
 
-void calcFinger(const int* tab, int n, int k, int* fingers);
+int ring_compare(void* a, void* b);
 
 
 #endif // CHORD_CONSTANTS
